6f94a6881957cb4a768ac1b3b2022c5335f9852e
selimssevgi <selimssevgi@gmail.com>
2018-08-30 11:25:27 +0300

    commons out spring test annotations

--- src/test/java/com/selimssevgi/wisit/wisitbe/rest/util/HealthCheckControllerIntTest.java
+++ src/test/java/com/selimssevgi/wisit/wisitbe/rest/util/HealthCheckControllerIntTest.java
@@ -1,27 +1,21 @@
 package com.selimssevgi.wisit.wisitbe.rest.util;
 
+import com.selimssevgi.wisit.wisitbe.AbstractControllerIntTest;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
-public class HealthCheckControllerIntTest {
+public class HealthCheckControllerIntTest extends AbstractControllerIntTest {
 
   @Autowired
   private MockMvc mockMvc;
 
   @Test
   public void shouldBeUp() throws Exception {
-    mockMvc.perform(get("/up"))
+    mockMvc.perform(get("/non-secured/up"))
         .andExpect(status().isOk());
   }
 }
