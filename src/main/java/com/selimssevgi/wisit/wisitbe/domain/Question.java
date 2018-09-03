3de8649f5e46730b9e5bba10a52e0cbd15f1f523
selimssevgi <selimssevgi@gmail.com>
2018-09-03 22:47:32 +0300

    config posgresql both for local and heroku

--- src/main/java/com/selimssevgi/wisit/wisitbe/domain/Question.java
+++ src/main/java/com/selimssevgi/wisit/wisitbe/domain/Question.java
@@ -1,11 +1,36 @@
 package com.selimssevgi.wisit.wisitbe.domain;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "questions")
 @Data
-@AllArgsConstructor
 public class Question {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  @Column(name = "img_url", nullable = false)
   private String imgUrl;
+
+  @Column(name = "location", nullable = false)
   private String location;
+
+  public Question(String imgUrl, String location) {
+    this.imgUrl = imgUrl;
+    this.location = location;
+  }
+
+  protected Question() {
+    // for jpa
+  }
 }
